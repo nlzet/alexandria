1e9f55dbee31ee36fd1c5332accd87f40115ffca
Niels Verbeek <niels.verbeek@adfinis.com>
2025-01-30 16:24:57 +0100

    feat(copy): add api endpoint to copy a document

--- alexandria/core/api.py
+++ alexandria/core/api.py
@@ -1,8 +1,10 @@
 import logging
+from os.path import splitext
 from typing import Optional, Tuple
 
 from django.conf import settings
 from django.core.files import File
+from django.utils.translation import gettext
 
 from alexandria.core import models, presign_urls
 from alexandria.core.validations import validate_file
@@ -33,6 +35,53 @@ def verify_signed_components(
     return presign_urls.verify_signed_components(
         pk, hostname, token_sig, expires, scheme, download_path
     )
+
+
+def copy_document(
+    document: models.Document, user: str, group: str, category: models.Category
+):
+    """
+    Copy a document and all its original files to a new document.
+
+    This function eases the copying of documents by automatically setting important fields.
+    Uses `create_file` to copy the original document files.
+    """
+
+    basename, ext = splitext(document.title)
+    document_title = gettext("{basename} (copy){ext}").format(
+        basename=basename, ext=ext
+    )
+    new_document = models.Document.objects.create(
+        title=document_title,
+        description=document.description,
+        metainfo=document.metainfo,
+        category=category,
+        created_by_user=user,
+        created_by_group=group,
+        modified_by_user=user,
+        modified_by_group=group,
+    )
+
+    # Copying only the originals - create_file() will create the thumbnails
+    document_files = models.File.objects.filter(
+        document=document, variant=models.File.Variant.ORIGINAL.value
+    ).order_by("created_at")
+    new_files = []
+    for document_file in document_files:
+        new_files.append(
+            create_file(
+                name=document_file.name,
+                document=new_document,
+                content=document_file.content,
+                mime_type=document_file.mime_type,
+                size=document_file.size,
+                user=document_file.created_by_user,
+                group=document_file.created_by_group,
+                metainfo=document_file.metainfo,
+            )
+        )
+
+    return new_document
 
 
 def create_document_file(
